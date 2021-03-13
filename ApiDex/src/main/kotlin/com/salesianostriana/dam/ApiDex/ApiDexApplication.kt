9164e9a8849486f235b36f2ba266fca82840a2e4
Pablo <pabloggm00@gmail.com>
2021-03-13 13:35:08 +0100

    created project

---
+++ ApiDex/src/main/kotlin/com/salesianostriana/dam/ApiDex/ApiDexApplication.kt
@@ -0,0 +1,11 @@
+package com.salesianostriana.dam.ApiDex
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiDexApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiDexApplication>(*args)
+}
---
+++ ApiDex/src/test/kotlin/com/salesianostriana/dam/ApiDex/ApiDexApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ApiDex
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiDexApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
