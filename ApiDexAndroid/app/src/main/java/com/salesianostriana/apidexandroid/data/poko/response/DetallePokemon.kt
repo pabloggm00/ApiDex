834371f967c20ef2b831fae6a79b7fc8d06a306d
Pablo <pabloggm00@gmail.com>
2021-03-21 18:00:28 +0100

    detallePokemonAñadido

---
+++ ApiDexAndroid/app/src/main/java/com/salesianostriana/apidexandroid/data/poko/response/DetallePokemon.kt
@@ -0,0 +1,9 @@
+package com.salesianostriana.apidexandroid.data.poko.response
+
+data class DetallePokemon(
+        val nombre: String,
+        val idPokedex: String,
+        val isFav: Boolean,
+        val isCapturado: Boolean,
+        //val imagen: Imagen
+)
--- ApiDexAndroid/app/src/main/java/com/salesianostriana/apidexandroid/ui/detallePokemon/DetallePokemonActivity.kt
+++ ApiDexAndroid/app/src/main/java/com/salesianostriana/apidexandroid/ui/detallePokemon/DetallePokemonActivity.kt
@@ -2,9 +2,14 @@ package com.salesianostriana.apidexandroid.ui.detallePokemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.MutableLiveData
 import com.salesianostriana.apidexandroid.R
 
 class DetallePokemonActivity : AppCompatActivity() {
+
+
+    private var _detallePokemon = MutableLiveData<De>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle_pokemon)
