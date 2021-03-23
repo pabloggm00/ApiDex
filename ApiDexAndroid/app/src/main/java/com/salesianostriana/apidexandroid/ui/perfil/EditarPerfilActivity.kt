0a127e0f1b720f28a8450478c8c99b5533caec7d
Pablo <pabloggm00@gmail.com>
2021-03-23 21:27:17 +0100

    casi terminado subir imagen

--- ApiDexAndroid/app/src/main/java/com/salesianostriana/apidexandroid/ui/perfil/EditarPerfilActivity.kt
+++ ApiDexAndroid/app/src/main/java/com/salesianostriana/apidexandroid/ui/perfil/EditarPerfilActivity.kt
@@ -3,20 +3,20 @@ package com.salesianostriana.apidexandroid.ui.perfil
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import coil.load
+import com.salesianostriana.apidexandroid.MainActivity
 import com.salesianostriana.apidexandroid.R
 import com.salesianostriana.apidexandroid.data.poko.response.UsuarioRegistroResponse
-import com.salesianostriana.apidexandroid.retrofit.PokemonService
 import com.salesianostriana.apidexandroid.retrofit.UsuarioService
 import okhttp3.MediaType
 import okhttp3.MultipartBody
@@ -28,18 +28,25 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
 
+
 class EditarPerfilActivity : AppCompatActivity() {
 
 
     private val baseUrl= "http://10.0.2.2:9000/"
     lateinit var service: UsuarioService
     lateinit var retrofit: Retrofit
+    val context = this
     var token: String? = ""
     private var _usuario = MutableLiveData<UsuarioRegistroResponse>()
 
     val PICK_IMAGE = 100
     val REQUEST_CODE = 100
     lateinit var body: MultipartBody.Part
+
+    val uriPathHelper = URIPathHelper()
+    lateinit var selectedImage: Uri
+     var filePath: String? = ""
+
 
     lateinit var btnGuardar: Button
     lateinit var editEmail: EditText
@@ -69,15 +76,7 @@ class EditarPerfilActivity : AppCompatActivity() {
         avatar = findViewById(R.id.imageViewEditAvatar)
         btnGuardar = findViewById(R.id.btn_guardar)
 
-
         getUser()
-
-        val file = File(_usuario.value!!.avatar)
-
-        val reqFile = RequestBody.create(MediaType.parse("image/*"), file)
-        body =
-                MultipartBody.Part.createFormData("file", file.name, reqFile)
-
 
 
         avatar.setOnClickListener(View.OnClickListener {
@@ -88,9 +87,12 @@ class EditarPerfilActivity : AppCompatActivity() {
 
         btnGuardar.setOnClickListener(View.OnClickListener {
             postImage()
-            var intent = Intent(this, PerfilViewModel::class.java)
+            var intent = Intent(this, MainActivity::class.java)
             this.startActivity(intent)
         })
+
+        val reqFile: RequestBody = RequestBody.create(MediaType.parse("image/*"), filePath!!)
+        body = MultipartBody.Part.createFormData("upload", filePath, reqFile)
 
 
 
@@ -98,8 +100,16 @@ class EditarPerfilActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE){
             avatar.setImageURI(data?.data) // handle chosen image
+            selectedImage = data?.getData()!!
+            filePath = context.let {uriPathHelper.getPath(context, selectedImage) }
+
+
+            println(selectedImage)
+            println(filePath)
+
+
         }
     }
 
@@ -111,12 +121,13 @@ class EditarPerfilActivity : AppCompatActivity() {
 
                     override fun onResponse(call: Call<UsuarioRegistroResponse>, response: Response<UsuarioRegistroResponse>) {
 
-                        if (response.code() == 200){
+                        if (response.code() == 200) {
                             _usuario.value = response.body()!!
 
                             textViewUsername.text = _usuario.value!!.username
                             editEmail.hint = _usuario.value!!.email
                             /*avatar.load(_usuario.value!!.avatar)*/
+
 
                         }
                     }
@@ -133,6 +144,9 @@ class EditarPerfilActivity : AppCompatActivity() {
                     call: Call<UsuarioRegistroResponse>,
                     response: Response<UsuarioRegistroResponse>
             ) {
+
+                println(response.code())
+
 
             }
 
